95486697baaa801a2c9879ba0792495ebb13b64d
Chris Sparshott <idioteque23@gmail.com>
2020-02-22 12:46:50 +0000

    only add google analytics if production

--- includes/google-analytics.php
+++ includes/google-analytics.php
@@ -1,3 +1,13 @@
+<?php
+
+$host = $_SERVER['HTTP_HOST'];
+$isLocal = (strpos($host, 'faye.test') !== false);
+
+if ($isLocal) {
+  return '';
+}
+
+?>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-6041643-54"></script>
 <script>
