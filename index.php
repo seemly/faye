2abc63d0c60b834322480c7337ee280cf6f43570
Chris Sparshott <idioteque23@gmail.com>
2020-03-07 11:27:57 +0000

    updaded home page to randomise cat images and display only one

--- index.php
+++ index.php
@@ -18,24 +18,38 @@
 
 <div id="media">
     <h2>Media</h2>
-    <p>Here are some images that I like.</p>
+    <p>RANDOM CAT!</p>
+
+    <?php
+
+    $images = [
+        [
+            'src' => "https://media.giphy.com/media/3o7qE7Hbomm69hYr04/giphy.gif",
+            'alt' => "A cute kitten saying how are you?",
+        ],
+        [
+            'src' => "https://media.giphy.com/media/VxbvpfaTTo3le/giphy.gif",
+            'alt' => "A ginger kitten flying in the sky",
+        ],
+        [
+            'src' => "https://media.giphy.com/media/uWYjSbkIE2XIMIc7gh/giphy.gif",
+            'alt' => "A kitten wearing a wizard hat and scarf",
+        ],
+        [
+            'src' => "https://media.giphy.com/media/U78wKLnlpdIQQgpNK2/giphy.gif",
+            'alt' => "A tiny kitten being brushed"
+        ]
+    ];
+
+    $imageCount = count($images);
+    $randomNumber = rand(0, $imageCount - 1);
+    ?>
 
     <div class="images">
         <div class="image">
-            <img src="https://media.giphy.com/media/3o7qE7Hbomm69hYr04/giphy.gif" alt="A cute kitten saying how are you?">
+            <img src="<?php echo $images[$randomNumber]["src"] ?>" alt="<?php echo $images[$randomNumber]["alt"] ?>">
         </div>
 
-        <div class="image">
-            <img src="https://media.giphy.com/media/VxbvpfaTTo3le/giphy.gif" alt="A ginger kitten flying in the sky">
-        </div>
-
-        <div class="image">
-            <img src="https://media.giphy.com/media/uWYjSbkIE2XIMIc7gh/giphy.gif" alt="A kitten wearing a wizard hat and scarf">
-        </div>
-
-        <div class="image">
-            <img src="https://media.giphy.com/media/U78wKLnlpdIQQgpNK2/giphy.gif" alt="A tiny kitten being brushed">
-        </div>
 
     </div>
 </div>
